46bf0f32b3d4e2ab1625f5cdb047f18030a2f5f7
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-04-29 18:21:40 +1000

    Create counter using useState and use client to use client component in page.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
+"use client";
+
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-2xl">
-        About
+      <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent">
+        Get Started
       </Link>
     </div>
   );
